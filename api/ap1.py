0715dc9cb88c0dcd9fb50a42b619259c85de0a23
ljmancini1 <lmancini@umbc.edu>
2022-05-15 11:25:19 -0400

    Add files via upload

---
+++ api/ap1.py
@@ -0,0 +1,48 @@
+# Dependencies
+from flask import Flask, request, jsonify
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+
+# Your API definition
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if model:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.DataFrame(json_)
+            query = query.reindex(columns=model_columns, fill_value=0)
+            print(query)
+            a = query.iloc[:,-1:]
+            query = query.iloc[:,:-1]
+            query = xgb.DMatrix(query, label=a)
+
+            print(model.predict(query))
+            prediction = list(model.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+    model = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
+    app.run(port=port, debug=True)
